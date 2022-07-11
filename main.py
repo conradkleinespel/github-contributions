91b4d2819e50c66ff961850ebbaeefe7ea64860a
Conrad Kleinespel <conradk@conradk.com>
2022-07-11 14:15:16 +0200

    Add possibility to backup individual commits without PR

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import datetime
-import json
 import requests
 from subprocess import getstatusoutput
 import os
 import sys
 from optparse import OptionParser
+import logging
 
 PER_PAGE = 100
 
 
-def fetch_pulls(author, page=1):
+def fetch_pulls(author, since=None, page=1):
     pulls = requests.get('https://api.github.com/search/issues', params={
-        "q": f"author:{author} sort:created-asc is:pr is:merged is:public",
+        "q": f"author:{author} sort:created-asc is:pr is:merged is:public" + (f" created:>={since}" if since else ""),
         "page": page,
         "per_page": PER_PAGE,
     }, headers={
@@ -37,17 +37,34 @@ def backup_pull(pull, output_dir):
     [_, _, _, organization, repo, _, pull_id] = pull['pull_request']['html_url'].split("/")
     pull_output_path = f"{date.year}-{date.month:02}-{date.day:02}-{organization}-{repo}-{pull_id}"
 
+    logging.info(f"Fetching pull request: {date} - {organization}/{repo}/{pull_id}")
+
     with \
-            open(f"{output_dir}/{pull_output_path}-conversation.html", "w") as html_conversation_file, \
-            open(f"{output_dir}/{pull_output_path}-commits.html", "w") as html_commits_file, \
-            open(f"{output_dir}/{pull_output_path}-files.html", "w") as html_files_file, \
-            open(f"{output_dir}/{pull_output_path}.diff", "w") as diff_file, \
-            open(f"{output_dir}/{pull_output_path}.patch", "w") as patch_file:
+            open(f"{output_dir}/pull-requests/{pull_output_path}-conversation.html", "w") as html_conversation_file, \
+            open(f"{output_dir}/pull-requests/{pull_output_path}-commits.html", "w") as html_commits_file, \
+            open(f"{output_dir}/pull-requests/{pull_output_path}-files.html", "w") as html_files_file, \
+            open(f"{output_dir}/pull-requests/{pull_output_path}.diff", "w") as diff_file, \
+            open(f"{output_dir}/pull-requests/{pull_output_path}.patch", "w") as patch_file:
         html_conversation_file.write(get_html_page(pull['pull_request']['html_url']))
         html_commits_file.write(get_html_page(f"{pull['pull_request']['html_url']}/commits"))
         html_files_file.write(get_html_page(f"{pull['pull_request']['html_url']}/files"))
         diff_file.write(requests.get(pull['pull_request']['diff_url'], allow_redirects=True).text)
         patch_file.write(requests.get(pull['pull_request']['patch_url'], allow_redirects=True).text)
+
+
+def backup_commit(commit_date, commit_url, output_dir):
+    [_, _, _, organization, repo, _, commit_hash] = commit_url.split("/")
+    commit_output_path = f"{commit_date}-{organization}-{repo}-{commit_hash}"
+
+    logging.info(f"Fetching commit: {commit_date} - {organization}/{repo}/{commit_hash}")
+
+    with \
+            open(f"{output_dir}/commits/{commit_output_path}-files.html", "w") as html_files_file, \
+            open(f"{output_dir}/commits/{commit_output_path}.diff", "w") as diff_file, \
+            open(f"{output_dir}/commits/{commit_output_path}.patch", "w") as patch_file:
+        html_files_file.write(get_html_page(f"{commit_url}"))
+        diff_file.write(requests.get(f"{commit_url}.diff", allow_redirects=True).text)
+        patch_file.write(requests.get(f"{commit_url}.patch", allow_redirects=True).text)
 
 
 def get_html_page(url):
@@ -58,10 +75,32 @@ def get_html_page(url):
     return html_output
 
 
+def create_directory(path):
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+
 def parse_args():
     parser = OptionParser(usage="usage: %prog [options] AUTHOR")
-    parser.add_option("-o", "--output-dir", dest="output_dir",
-                      help="Directory to write pull request content in", metavar="OUTPUT_DIR", default='./output')
+    parser.add_option(
+        "-o",
+        dest="output_dir",
+        help="Directory to write pull request content in",
+        metavar="OUTPUT_DIR",
+        default='./output',
+    )
+    parser.add_option(
+        "-c",
+        dest="commits_listing",
+        help="File in which to list additional commits to backup",
+        metavar="COMMITS_LISTING",
+    )
+    parser.add_option(
+        "-s",
+        dest="since",
+        help="Only search pull requests created since date (yyyy-mm-dd)",
+        metavar="SINCE_DATE",
+    )
 
     (options, args) = parser.parse_args()
 
@@ -75,11 +114,22 @@ def parse_args():
 
 
 def main():
+    logging.basicConfig(encoding='utf-8', level=logging.INFO)
+
     (options, author) = parse_args()
 
-    pulls = fetch_pulls(author)
+    create_directory(f"{options.output_dir}/pull-requests")
+    create_directory(f"{options.output_dir}/commits")
+
+    pulls = fetch_pulls(author, options.since)
     for pull in pulls:
         backup_pull(pull, options.output_dir)
+
+    if options.commits_listing:
+        with open(options.commits_listing, 'r') as commits_listing:
+            commits = [commit for commit in commits_listing.read().split('\n') if commit]
+            for [commit_date, commit_url] in [commit.split(' ') for commit in commits]:
+                backup_commit(commit_date, commit_url, options.output_dir)
 
 
 if __name__ == '__main__':
