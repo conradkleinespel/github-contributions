610a0edae5faf0a268d26f820d1e5bac2b3af722
Conrad Kleinespel <conradk@conradk.com>
2022-07-11 23:41:51 +0200

    Add possibility to backup individual issues without PR

--- main.py
+++ main.py
@@ -52,6 +52,16 @@ def backup_pull(pull, output_dir):
         patch_file.write(requests.get(pull['pull_request']['patch_url'], allow_redirects=True).text)
 
 
+def backup_issue(issue_date, issue_url, output_dir):
+    [_, _, _, organization, repo, _, issue_id] = issue_url.split("/")
+    issue_output_path = f"{issue_date}-{organization}-{repo}-{issue_id}"
+
+    logging.info(f"Fetching issue: {issue_date} - {organization}/{repo}/{issue_id}")
+
+    with open(f"{output_dir}/issues/{issue_output_path}.html", "w") as html_files_file:
+        html_files_file.write(get_html_page(f"{issue_url}"))
+
+
 def backup_commit(commit_date, commit_url, output_dir):
     [_, _, _, organization, repo, _, commit_hash] = commit_url.split("/")
     commit_output_path = f"{commit_date}-{organization}-{repo}-{commit_hash}"
@@ -96,6 +106,12 @@ def parse_args():
         metavar="COMMITS_LISTING",
     )
     parser.add_option(
+        "-i",
+        dest="issues_listing",
+        help="File in which to list additional issues to backup",
+        metavar="ISSUES_LISTING",
+    )
+    parser.add_option(
         "-s",
         dest="since",
         help="Only search pull requests created since date (yyyy-mm-dd)",
@@ -119,6 +135,7 @@ def main():
     (options, author) = parse_args()
 
     create_directory(f"{options.output_dir}/pull-requests")
+    create_directory(f"{options.output_dir}/issues")
     create_directory(f"{options.output_dir}/commits")
 
     pulls = fetch_pulls(author, options.since)
@@ -130,6 +147,12 @@ def main():
             commits = [commit for commit in commits_listing.read().split('\n') if commit]
             for [commit_date, commit_url] in [commit.split(' ') for commit in commits]:
                 backup_commit(commit_date, commit_url, options.output_dir)
+
+    if options.issues_listing:
+        with open(options.issues_listing, 'r') as issues_listing:
+            issues = [issue for issue in issues_listing.read().split('\n') if issue]
+            for [issue_date, issue_url] in [issue.split(' ') for issue in issues]:
+                backup_issue(issue_date, issue_url, options.output_dir)
 
 
 if __name__ == '__main__':
